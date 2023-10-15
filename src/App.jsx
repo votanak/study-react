426101fbdff1eebf83f7b52bbe290627b4c771ee
votanak <v0tanak@yandex.ru>
2023-10-15 03:05:01 +0300

    asdfasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,77 @@
 import { useState } from "react";
 
+// export function App() {
+// 	const [notes, setNotes] = useState(["a", "b", "c", "d", "e"]);
+
+// 	function changeHandler(event) {
+// 		setNotes([...notes.slice(0, 1), event.target.value, ...notes.slice(1 + 1)]);
+// 	}
+
+// 	const result = notes.map((note, index) => {
+// 		<li>
+// 			{notes[1]} &nbsp; &nbsp;
+// 			<button>Change</button>
+// 		</li>;
+// 	});
+
+// 	return (
+// 		<div>
+// 			<ul>{result}</ul>
+// 			<input id="input" onChange={changeHandler} value={notes[1]}></input>
+// 		</div>
+// 	);
+// }
+
 export function App() {
 	const [notes, setNotes] = useState(["a", "b", "c", "d", "e"]);
 	const [changeIndex, setChangeIndex] = useState();
+	const [value, setValue] = useState();
+	console.log(changeIndex, typeof changeIndex === "number");
 
-	function changeHandler(index) {
-		setNotes([
-			...notes.slice(0, index),
-			notes[index],
-			...notes.slice(index + 1),
-		]);
+	function changeHandler(event) {
+		if (typeof changeIndex === "number") {
+			setNotes([
+				...notes.slice(0, changeIndex),
+				event.target.value,
+				...notes.slice(changeIndex + 1),
+			]);
+		} else {
+			setValue(event.target.value);
+		}
 	}
+
+	const clickHandler = (event, index) => {
+		event.preventDefault();
+		document.getElementById("input").focus();
+		setChangeIndex(index);
+	};
 
 	const result = notes.map((note, index) => {
 		return (
-			<div key={index}>
-				<li>
-					{note}
-					<button onClick={() => setChangeIndex(changeIndex)}>Change</button>
-				</li>
-			</div>
+			<li key={index} onClick={(event) => clickHandler(event, index)}>
+				{note} &nbsp; &nbsp;
+			</li>
 		);
 	});
+
+	const blurHandle = () => {
+		if (typeof changeIndex === "number") {
+			setChangeIndex(undefined);
+		} else {
+			setNotes([...notes, value]);
+		}
+		setValue("");
+	};
 
 	return (
 		<div>
 			<ul>{result}</ul>
-			<input onChange={() => changeHandler(changeIndex)}></input>
+			<input
+				id="input"
+				onChange={changeHandler}
+				onBlur={blurHandle}
+				value={typeof changeIndex === "number" ? notes[changeIndex] : value}
+			></input>
 		</div>
 	);
 }
