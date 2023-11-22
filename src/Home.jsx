c20ec545ef69254be2b2df636f5aaa22ecdd5bd1
votanak <v0tanak@yandex.ru>
2023-11-22 23:56:43 +0300

    lskdjf

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,46 @@
 import React from "react";
 import { Slider } from "./components/Slider";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import lion from "./components/lion.jpg";
 
 export const Home = () => {
   return (
     <>
       <Slider />
+      <Container style={{ paddingTop: "2rem", paddingBottom: "2rem" }}>
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={lion} />
+              <Card.Body>
+                <Card.Title>My Card</Card.Title>
+                <Card.Text>lorem lskdjf aslkjdal kdjalskdfj alskdjf </Card.Text>
+                <Button variant="primary">Learn more...</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={lion} />
+              <Card.Body>
+                <Card.Title>My Card</Card.Title>
+                <Card.Text>lorem lskdjf aslkjdal kdjalskdfj alskdjf </Card.Text>
+                <Button variant="primary">Learn more...</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={lion} />
+              <Card.Body>
+                <Card.Title>My Card</Card.Title>
+                <Card.Text>lorem lskdjf aslkjdal kdjalskdfj alskdjf </Card.Text>
+                <Button variant="primary">Learn more...</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
--- src/components/Navibar.jsx
+++ src/components/Navibar.jsx
@@ -1,6 +1,6 @@
-import { React } from "react";
-import { Navbar, Nav, Button, Container } from "react-bootstrap";
-import { Routes, Route, Link } from "react-router-dom";
+import { React, useState } from "react";
+import { Navbar, Nav, Button, Container, Modal, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const NaviBarStyle = styled.div`
@@ -16,35 +16,69 @@ const NaviBarStyle = styled.div`
 `;
 
 export const NaviBar = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+  };
+  const handleShow = () => {
+    setShow(true);
+  };
+
   return (
-    <NaviBarStyle>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>Web Dev Blog</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link>
-                <Link to="/">Home</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/users">Users</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/about">About</Link>
-              </Nav.Link>
-            </Nav>
-            <Nav>
-              <Button variant="primary" className="me-2">
-                Log In
-              </Button>
-              <Button variant="primary" className="me-2">
-                Sign Out
-              </Button>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </NaviBarStyle>
+    <>
+      <NaviBarStyle>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand>Web Dev Blog</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link>
+                  <Link to="/">Home</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/users">Users</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/about">About</Link>
+                </Nav.Link>
+              </Nav>
+              <Nav>
+                <Button variant="primary" className="me-2" onClick={handleShow}>
+                  Log In
+                </Button>
+                <Button variant="primary" className="me-2" onClick={handleShow}>
+                  Sign Out
+                </Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </NaviBarStyle>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Log In</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>E-mail adress</Form.Label>
+              <Form.Control type="email" placeholder="e-mail" />
+              <Form.Text className="text-muted">
+                We'll never Share e-mail
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>E-mail adress</Form.Label>
+              <Form.Control type="password" placeholder="password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicChekbox">
+              <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
